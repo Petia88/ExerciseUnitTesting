d676f2dd64675f428b6cdf86860aa829aae631d3
Petia Mladenova <135381775+Petia88@users.noreply.github.com>
2024-05-17 16:33:21 +0300

    Add project files.

---
+++ TestApp.UnitTests/EmailTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailTests
+{
+    
+    [Test]
+    public void Test_IsValidEmail_ValidEmail()
+    {
+        // Arrange
+        string validEmail = "test@example.com";
+
+        // Act
+        bool isValidEmail = Email.IsValidEmail(validEmail);
+
+        // Assert
+        Assert.That(isValidEmail, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void Test_IsValidEmail_InvalidEmail()
+    {
+        // Arrange
+        string invalidMail = "test.example.com";
+
+        // Act
+        bool isValidEmail = Email.IsValidEmail(invalidMail);
+
+        // Assert
+        Assert.IsFalse(isValidEmail);
+    }
+
+    [Test]
+    public void Test_IsValidEmail_NullInput()
+    {
+        // Arrange
+        string nullMail = null;
+
+        // Act
+        bool isValidEmail = Email.IsValidEmail(nullMail);
+
+        // Assert
+        Assert.IsFalse(isValidEmail);
+    }
+}
---
+++ TestApp.UnitTests/FakeTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class FakeTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_RemoveStringNumbers_NullInput_ThrowsException()
+    {
+        // Arrange
+        char[]? input = null;
+
+        // Act + Assert
+        Assert.That(() => Fake.RemoveStringNumbers(input), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_RemoveStringNumbers_RemovesDigitsFromCharArray()
+    {
+        //Arange
+        char[] input = new char[] { '0', '1', 'a', 'b' };
+
+        //Act
+        char[] result = Fake.RemoveStringNumbers(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(new[] { 'a', 'b' }));
+    }
+
+    [Test]
+    public void Test_RemoveStringNumbers_NoDigitsInInput_ReturnsSameArray()
+    {
+        //Arange
+        char[] input = new char[] { 'c', 'd', 'a', 'b' };
+
+        //Act
+        char[] result = Fake.RemoveStringNumbers(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(input));
+    }
+
+    [Test]
+    public void Test_RemoveStringNumbers_EmptyArray_ReturnsEmptyArray()
+    {
+        //Arange
+        char[] input = Array.Empty<char>();
+
+        //Act
+        char[] result = Fake.RemoveStringNumbers(input);
+
+        //Assert
+        Assert.That(result, Is.Empty);
+    }
+}
